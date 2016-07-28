31116621d6f1600ea912f8ecbf9f1f2d2e0549f0
xpp <1522374636@qq.com>
2016-07-28 20:33:13 +0800

    normal commit

--- app/src/main/java/com/lxkj/administrator/fealty/bean/UserInfo.java
+++ app/src/main/java/com/lxkj/administrator/fealty/bean/UserInfo.java
@@ -3,10 +3,116 @@ package com.lxkj.administrator.fealty.bean;
 import android.graphics.Bitmap;
 import android.widget.ImageView;
 
+import java.io.Serializable;
+
 /**
  * Created by Administrator on 2016/7/27.
  */
-public class UserInfo {
+public class UserInfo implements Serializable{
     public String userName;
     public Bitmap bitmap;
+
+    //用户类型标杆 1.USER_OLDMAN_CODE_MAN ,老人 2.USER_YUNGMAN_CODE_WOMAN子女
+    public static final String USER_OLDMAN_CODE_MAN = "1", USER_YUNGMAN_CODE_WOMAN = "2";
+    private String gender;
+    private String userid;
+    private String mobile;//掩码
+    private String mobilePlain;//明码
+    private String nickName;//昵称
+    private String username;//真实姓名
+    private String birthday;
+    private String userpic;
+
+    /**
+     * 登录
+     */
+    protected void login() {
+
+    }
+
+    /**
+     * 退出登录
+     */
+    protected void logout() {
+
+    }
+
+    /**
+     * 注册
+     */
+    protected void regist() {
+
+    }
+
+    /**
+     * 修改密码
+     */
+
+    protected void changePassword() {
+
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getMobilePlain() {
+        return mobilePlain;
+    }
+
+    public void setMobilePlain(String mobilePlain) {
+        this.mobilePlain = mobilePlain;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getUserpic() {
+        return userpic;
+    }
+
+    public void setUserpic(String userpic) {
+        this.userpic = userpic;
+    }
 }
--- app/src/main/java/com/lxkj/administrator/fealty/fragment/LoginFragment.java
+++ app/src/main/java/com/lxkj/administrator/fealty/fragment/LoginFragment.java
@@ -1,11 +1,18 @@
 package com.lxkj.administrator.fealty.fragment;
 
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lxkj.administrator.fealty.R;
 import com.lxkj.administrator.fealty.base.BaseFragment;
 import com.lxkj.administrator.fealty.event.NavFragmentEvent;
+import com.lxkj.administrator.fealty.utils.ToastUtils;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
@@ -20,11 +27,33 @@ import de.greenrobot.event.EventBus;
 public class LoginFragment extends BaseFragment {
     @ViewInject(R.id.tv_login)
     private TextView tv_login;
+    @ViewInject(R.id.tv_forgetpw)
+    private TextView tv_forgerpw;
+    @ViewInject(R.id.tv_regist)
+    private TextView tv_regist;
+    private String phone;
+    private String password;
+    @ViewInject(R.id.login_et_phone)
+    private EditText login_phone_edittext;
+    @ViewInject(R.id.login_et_password)
+    private EditText login_password_edittext;
+    public static final int REQUEST_CODE_LOGIN_COMMIT = 0X12;
+    public static final int REQUEST_CODE_UPLOAD_FRIEND = 0X20;
+    private final int REQUIRE_CODE_CHEEK_SESSION = 0X14;
+    private final int REQUIRE_CODE_READ_CONTACT_FROM_SP = 0X13;
+    private boolean showPassword;
 
-    //    protected View initView(LayoutInflater inflater, ViewGroup container) {
-//        View view= inflater.inflate(R.layout.fragment_login, container, false);
-//        return view;
-//    }
+    private long beforeTime;
+    private long delayTime;//最短睡眠时间
+    private long afterTime;
+
+    public static final int MESSAGE_WHAT_LOGIN_LOGINING = 1;
+    public static final int MESSAGE_WHAT_LOGIN_LOGINSUCCESS = 2;
+    public static final int MESSAGE_WHAT_LOGIN_LOGINFAIL = 3;
+    public static final int MESSAGE_WHAT_LOGIN_COMMIT = 0X10;
+    private static long DEFAULTTIME = 500;//登录过程最少持续时间
+    @ViewInject(R.id.login_iv_password_see_or_hidden)
+    private ImageView seePasswordImageView;
     @Override
     protected void init() {
 
@@ -32,16 +61,31 @@ public class LoginFragment extends BaseFragment {
 
     @Override
     protected void initListener() {
-//        tv_login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                goToMainTabsClick();
-//            }
-//        });
+seePasswordImageView.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if (showPassword) {
+           login_password_edittext.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            seePasswordImageView.setImageResource(R.mipmap.icon_login_show_password_normal);
+        } else {
+          login_password_edittext.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            seePasswordImageView.setImageResource(R.mipmap.icon_login_show_password_active);
+        }
+        showPassword = !showPassword;
+    }
+});
     }
 
+    /**
+     * 登录
+     *
+     * @param view
+     */
     @Event(R.id.tv_login)
     private void goToMainTabsClick(View view) {
+        phone = login_phone_edittext.getText().toString().trim();
+        password = login_password_edittext.getText().toString().trim();
+        doLogin(phone, password);
 //        switch (view.getId()){
 //            case R.id.tv_login:
 //                break;
@@ -50,15 +94,38 @@ public class LoginFragment extends BaseFragment {
 //            case R.id.tv_forgetpw:
 //                break;
 //        }
-        MainTabsFragemnt mainTabsFragemnt = new MainTabsFragemnt();
-        EventBus.getDefault().post(new NavFragmentEvent(mainTabsFragemnt));
+
+    }
+
+    /**
+     * 处理登录的方法
+     *
+     * @param phone
+     * @param password
+     */
+    private void doLogin(String phone, String password) {
+        if (TextUtils.isEmpty(phone)) {
+            //  Toast.makeText(getApplicationContext(), "输入手机号不能为空！", Toast.LENGTH_SHORT).show();
+            ToastUtils.showToastInUIThread("输入手机号不能为空！");
+        } else if (TextUtils.isEmpty(password)) {
+            //  Toast.makeText(getApplicationContext(), "输入密码不能为空！", Toast.LENGTH_SHORT).show();
+            ToastUtils.showToastInUIThread("输入密码不能为空！");
+        } else {
+            //  handler.sendEmptyMessage(MESSAGE_WHAT_LOGIN_LOGINING);//通知UI ，正在执行登录联网操作
+            // beforeTime = System.currentTimeMillis();
+            // userid = CommonTools.encodeByMD5(phone);
+            //RequireServiceManager.getRequireServiceManager().requireToLogin(this, userid, password, REQUEST_CODE_LOGIN_COMMIT, this);
+            MainTabsFragemnt mainTabsFragemnt = new MainTabsFragemnt();
+            EventBus.getDefault().post(new NavFragmentEvent(mainTabsFragemnt));
+        }
     }
 
     @Event(R.id.tv_forgetpw)
     private void goToResetPassword(View view) {
-       ResetPasswordFragment resetPasswordFragment=new ResetPasswordFragment();
+        ResetPasswordFragment resetPasswordFragment = new ResetPasswordFragment();
         EventBus.getDefault().post(new NavFragmentEvent(resetPasswordFragment));
     }
+
     @Event(R.id.tv_regist)
     private void goToRegist(View view) {
         RegistFragment registFragment = new RegistFragment();
